07a97780c18bbabab15d364e2051cd020855cc29
Jose Garcia <jg03298@gmail.com>
2025-08-08 20:17:55 -0700

    Removed comment

--- server/src/main/java/com/test/Main.java
+++ server/src/main/java/com/test/Main.java
@@ -4,8 +4,6 @@ import com.test.server.Server;
 
 import java.io.IOException;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         try {
